f30d0be3d915abbda0ff17a26e1df47f353e621e
CoroDaniel <danielcoroian15@gmail.com>
2021-05-05 14:31:54 +0300

    edit header

--- src/components/header-page.php
+++ src/components/header-page.php
@@ -8,7 +8,7 @@
                 <li><a href="../index.php" class="push-left">Account</a></li>
                 <!-- <li>"><a href="../../pages/index.html">Username</a></li> -->
                 <li><a href="./books.php">My Books</a></li>
-                <li><a href="../index.php">Add Book</a></li>
+                <li><a href="./insert.php">Add Book</a></li>
                 <!-- <li id="logout"><a href="../../pages/index.html">Logout</a></li> -->
             </ul>
 EOT;
